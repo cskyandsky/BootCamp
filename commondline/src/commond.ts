cc0b538fc3a4bec0400c9c33f5330ba7d3df4a4e
cskyandsky <161993042+cskyandsky@users.noreply.github.com>
2025-04-05 15:44:36 -0400

    update to mcp-server

--- commondline/src/commond.ts
+++ commondline/src/commond.ts
@@ -1,27 +1,35 @@
 
 import { exec } from "child_process";
 import { promisify } from "util";
-import { string } from "zod";
 
 const execPromise = promisify(exec);
 
-export async function runCommand({ cmd }: { cmd: string; }): Promise<{ stdout:string, stderr: string }> {
+/**
+ * 
+ * @param cmd - The command to run in the shell
+ * @returns A promise that resolves to an object with stdout and stderr properties
+ */
+
+export async function runCommand(
+    cmd :  string
+  ): Promise<{ stdout: string, stderr: string }> {
     try {
         const { stdout, stderr } = await execPromise(cmd);
         
-        return { stdout: stdout.trim(), stderr: stderr.trim()}; // Remove extra whitespace
-    } catch (error) {
+        return { stdout: stdout.trim(), stderr: stderr.trim() }; // Remove extra whitespace
+    } catch (error: unknown) {
         console.error (`Error excuting commond: ${cmd}`);
         console.error(error);
         let errorMessage = "Unknown error occurred";
+
         if (error instanceof Error) {
           errorMessage =error.message;
         } else if (typeof error === 'string') {
           errorMessage = error;
         }else if (error && typeof error ==='object' && 'message' in error) {
-          errorMessage = String(error.message)
+          errorMessage = (error as { message: string}).message;
         }
-        return {stdout:"", stderr: error.message};
+        return { stdout: "", stderr: errorMessage};
     }
 }
 
